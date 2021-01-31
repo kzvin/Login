5c8717e593cbde0127e263b8fa66c54524961d74
kzvin <74388378+kzvin@users.noreply.github.com>
2021-01-31 16:25:25 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,101 @@
-<!doctype html>
-<html>
-    <head>
-        <link rel="shortcut icon" href="#" />
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Login con  PHP - Bootstrap 4</title>
+<?php require_once "vistas/parte_superior.php"?>
 
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="estilos.css">
-        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
-        
-        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
-        
-    </head>
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Contenido principal</h1>
     
-    <body>
-     
-      <div class="container-login">
-        <div class="wrap-login">
-            <form class="login-form validate-form" id="formLogin" action="" method="post">
-                <span class="login-form-title">LOGIN</span>
-                
-                <div class="wrap-input100" data-validate = "Usuario incorrecto">
-                    <input class="input100" type="text" id="usuario" name="usuario" placeholder="Usuario">
-                    <span class="focus-efecto"></span>
-                </div>
-                
-                <div class="wrap-input100" data-validate="Password incorrecto">
-                    <input class="input100" type="password" id="password" name="password" placeholder="Password">
-                    <span class="focus-efecto"></span>
-                </div>
-                
-                <div class="container-login-form-btn">
-                    <div class="wrap-login-form-btn">
-                        <div class="login-form-bgbtn"></div>
-                        <button type="submit" name="submit" class="login-form-btn">CONECTAR</button>
+    
+    
+ <?php
+include_once '/bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$consulta = "SELECT id, nombre, pais, edad FROM personas";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>    
+        </div>    
+    </div>    
+    <br>  
+<div class="container">
+        <div class="row">
+                <div class="col-lg-12">
+                    <div class="table-responsive">        
+                        <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
+                        <thead class="text-center">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nombre</th>
+                                <th>País</th>                                
+                                <th>Edad</th>  
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php                            
+                            foreach($data as $dat) {                                                        
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['nombre'] ?></td>
+                                <td><?php echo $dat['pais'] ?></td>
+                                <td><?php echo $dat['edad'] ?></td>    
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>        
+                       </table>                    
                     </div>
                 </div>
-            </form>
+        </div>  
+    </div>    
+      
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <form id="formPersonas">    
+            <div class="modal-body">
+                <div class="form-group">
+                <label for="nombre" class="col-form-label">Nombre:</label>
+                <input type="text" class="form-control" id="nombre">
+                </div>
+                <div class="form-group">
+                <label for="pais" class="col-form-label">País:</label>
+                <input type="text" class="form-control" id="pais">
+                </div>                
+                <div class="form-group">
+                <label for="edad" class="col-form-label">Edad:</label>
+                <input type="number" class="form-control" id="edad">
+                </div>            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
         </div>
-    </div>     
-        
-        
-     <script src="jquery/jquery-3.3.1.min.js"></script>    
-     <script src="bootstrap/js/bootstrap.min.js"></script>    
-     <script src="popper/popper.min.js"></script>    
-        
-     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
-     <script src="codigo.js"></script>    
-    </body>
-</html>
+    </div>
+</div>  
+      
+    
+    
+</div>
+<!--FIN del cont principal-->
+<?php require_once "vistas/parte_inferior.php"?>
